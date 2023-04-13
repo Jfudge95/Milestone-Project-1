26b4fca1d0c7de0d3f05fbb5b13fff6fea55d12b
Jordan Fudge <jordanfudge95@gmail.com>
2023-04-13 13:58:50 -0400

    Added Platforms in JS

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const character = document.createElement("div");
   let characterLeftSpace = 50;
   let characterBottomSpace = 150;
+  let isGameOver = false;
+  let platformCount = 5;
 
   function createCharacter() {
     map.appendChild(character);
@@ -11,9 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
     character.style.bottom = characterBottomSpace + "px";
   }
 
+  class Platform {
+    constructor(newPlatBottom) {
+      this.bottom = newPlatBottom;
+      this.left = Math.random() * 315;
+      this.visual = document.createElement("div");
+
+      const visual = this.visual;
+      visual.classList.add("platform");
+      visual.style.left = this.left + "px";
+      visual.style.bottom = this.bottom + "px";
+      map.appendChild(visual);
+    }
+  }
+
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+      let platformGap = 800 / platformCount;
+      let newPlatBottom = 100 + i * platformGap;
+      let newPlatform = new Platform(newPlatBottom);
+    }
+  }
+
   function start() {
     if (isGameOver == false) {
       createCharacter();
+      createPlatforms();
     }
   }
   //attach to button
