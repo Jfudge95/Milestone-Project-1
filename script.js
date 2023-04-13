adc2ccd5771495eeea767511e0da9e253ef4fcc3
Jordan Fudge <jordanfudge95@gmail.com>
2023-04-13 14:35:58 -0400

    Created Movement in our Platforms

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const map = document.querySelector(".map");
   const character = document.createElement("div");
   let characterLeftSpace = 50;
-  let characterBottomSpace = 150;
+  let characterBottomSpace = 250;
   let isGameOver = false;
   let platformCount = 5;
+  let platforms = [];
 
   function createCharacter() {
     map.appendChild(character);
@@ -32,13 +33,27 @@ document.addEventListener("DOMContentLoaded", () => {
       let platformGap = 800 / platformCount;
       let newPlatBottom = 100 + i * platformGap;
       let newPlatform = new Platform(newPlatBottom);
+      platforms.push(newPlatform);
     }
   }
+  function movePlatforms() {
+    if (characterBottomSpace > 200) {
+      platforms.forEach((platform) => {
+        platform.bottom -= 4;
+        let visual = platform.visual;
+        visual.style.bottom = platform.bottom + "px";
+      });
+    }
+  }
+
+  function jump() {}
 
   function start() {
     if (isGameOver == false) {
       createCharacter();
       createPlatforms();
+      setInterval(movePlatforms, 30);
+      jump();
     }
   }
   //attach to button
